8165e4623448d3a6cf8d0875d795959dfa2289b0
ugCring <pengxiao13613@163.com>
2022-11-28 16:43:03 +0800

    Create the project.

---
+++ src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Author: PX
+ * @Date: 2022-11-28 14:31:52
+ * @LastEditTime: 2022-11-28 15:16:24
+ * @LastEditors: PX
+ * @Description:  
+-->
+<template>
+  <h1>Salt:</h1>
+  <input class="salt-input" type="text" placeholder="" />
+</template>
+
+<style lang="less">
+.salt-input {
+  padding: 0 10px;
+  outline: none;
+  font-size: 30px;
+  height: 60px;
+  border: 1px solid var(--vt-c-text-light-1);
+  border-radius: 12px;
+}
+</style>
