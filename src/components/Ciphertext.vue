6244cfc409b339fadc39fc3edccf3383d64453a0
ugCring <pengxiao13613@163.com>
2022-11-30 15:11:00 +0800

    v1

--- src/components/Ciphertext.vue
+++ src/components/Ciphertext.vue
@@ -2,40 +2,26 @@
   <div class="cipher-comp">
     <div class="row salt">
       <span class="label salt-label">Salt:</span>
-      <input
-        class="input salt-input"
-        type="text"
-        placeholder=""
-        v-model="salt"
-      />
+      <input class="input salt-input" type="text" placeholder="" v-model="salt" @keyup.enter="getCiphertext" />
     </div>
     <div class="row salt">
       <span class="label salt-label">Seed:</span>
-      <input
-        class="input salt-input"
-        type="text"
-        placeholder=""
-        v-model="seed"
-      />
+      <input class="input salt-input" type="text" placeholder="" v-model="seed" @keyup.enter="getCiphertext" />
     </div>
     <div class="row">
-      <button
-        :class="['btn', btnDisabled ? 'disabled' : '']"
-        @click="getCiphertext"
-      >
+      <button :class="['btn', btnDisabled ? 'disabled' : '']" @click="getCiphertext">
         {{
-          btnDisabled
-            ? 'Please enter salt value and seed'
-            : 'Click To Get Your Ciphertext'
+            btnDisabled
+              ? 'Please enter salt value and seed'
+              : 'Click To Get Your Ciphertext'
         }}
       </button>
     </div>
     <div class="row">
-      <span class="label wrap"
-        >Your Ciphertext: <span class="tip">(Click To Copy)</span></span
-      >
+      <span class="label wrap">Your Ciphertext: <span class="tip">(Click To Copy)</span></span>
       <div class="seed" @click="copySeed">
         {{ ciphertext }}
+        <div class="copy-tip" v-if="showCopyTip">Copied!</div>
       </div>
     </div>
   </div>
@@ -54,15 +40,22 @@ function getCiphertext() {
   try {
     const seedBase64 = btoa(encodeURIComponent(seed.value))
     const saltBase64Part = btoa(encodeURIComponent(salt.value)).replace(/=/g, '')
-    const splitIndex = Math.floor(Math.random() * 10) % seedBase64.length
-    let mix = seedBase64.slice(0, splitIndex) + saltBase64Part + seedBase64.slice(splitIndex)
-    console.log('mix', mix)
-    ciphertext.value = btoa(mix)
+    const saltReverseBase64Part = btoa(encodeURIComponent(salt.value).split('').reverse().join('')).replace(/=/g, '')
+
+    const splitIndex1 = Math.floor(Math.random() * 1000) % seedBase64.length
+    const mix1 = seedBase64.slice(0, splitIndex1) + saltBase64Part + seedBase64.slice(splitIndex1)
+    const mix1Base64 = btoa(mix1)
+
+    const splitIndex2 = Math.floor(Math.random() * 1000) % mix1Base64.length
+    const mix2 = mix1Base64.slice(0, splitIndex2) + saltReverseBase64Part + mix1Base64.slice(splitIndex2)
+
+    ciphertext.value = btoa(mix2)
   } catch (err) {
     alert(err.message)
   }
 }
 
+let showCopyTip = ref(false)
 function copySeed() {
   if (!ciphertext.value) {
     alert('Please get your ciphertext first.')
@@ -75,7 +68,10 @@ function copySeed() {
   document.getElementById('cp_zdy_input').select()
   document.execCommand('copy')
   document.getElementById('cp_zdy_input').remove()
-  alert('Successfully Copied')
+  showCopyTip.value = true
+  setTimeout(() => {
+    showCopyTip.value = false
+  }, 2000);
 }
 </script>
 
@@ -84,34 +80,40 @@ function copySeed() {
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
   width: 100%;
+  min-height: 600px;
   .row {
     display: flex;
-    align-items: center;
+    // align-items: center;
     justify-content: space-between;
+
     @media screen and (min-width: 962px) {
       margin-bottom: 30px;
       width: 600px;
     }
+
     @media screen and (max-width: 962px) {
       margin-bottom: 20px;
       flex-direction: column;
       width: 100%;
     }
+
     .label {
       font-size: 20px;
       display: flex;
       justify-content: flex-start;
       align-items: center;
+      height: 40px;
       @media screen and (min-width: 962px) {
         flex: 0 0 170px;
       }
+
       @media screen and (max-width: 962px) {
         width: 100%;
         margin-bottom: 5px;
       }
     }
+
     .input {
       padding: 0 10px;
       outline: none;
@@ -119,13 +121,16 @@ function copySeed() {
       height: 40px;
       border: 1px solid #000;
       border-radius: 12px;
+
       @media screen and (min-width: 962px) {
         flex: 1;
       }
+
       @media screen and (max-width: 962px) {
         width: 100%;
       }
     }
+
     .btn {
       font-size: 20px;
       height: 40px;
@@ -134,37 +139,46 @@ function copySeed() {
       border-radius: 12px;
       outline: none;
       background-color: #000;
+
       @media screen and (min-width: 962px) {
         flex: 1;
       }
+
       @media screen and (max-width: 962px) {
         width: 100%;
       }
+
       &:active {
         background-color: #fff;
         outline: none;
         color: #000;
       }
     }
+
     .disabled {
       cursor: not-allowed;
       border-color: #000 !important;
       font-size: 16px;
+
       @media screen and (max-width: 962px) {
         font-size: 14px;
       }
+
       &:active {
         background-color: #000;
         color: #fff;
       }
     }
+
     .wrap {
       flex-wrap: wrap;
+
       .tip {
         white-space: nowrap;
         font-size: 15px;
       }
     }
+
     .seed {
       word-break: break-all;
       background-color: rgb(239, 239, 239);
@@ -173,11 +187,20 @@ function copySeed() {
       border: 1px solid #000;
       padding: 12px;
       min-height: 40px;
+      position: relative;
+
       @media screen and (min-width: 962px) {
         flex: 1;
       }
+
       @media screen and (max-width: 962px) {
         width: 100%;
+      }
+
+      .copy-tip {
+        position: absolute;
+        left: 0;
+        bottom: -30px;
       }
     }
   }
--- src/components/Seed.vue
+++ src/components/Seed.vue
@@ -2,37 +2,26 @@
   <div class="seed-comp">
     <div class="row salt">
       <span class="label salt-label">Salt:</span>
-      <input
-        class="input salt-input"
-        type="text"
-        placeholder=""
-        v-model="salt"
-      />
+      <input class="input salt-input" type="text" placeholder="" v-model="salt" @keyup.enter="getSeed" />
     </div>
     <div class="row salt">
       <span class="label salt-label">Ciphertext:</span>
-      <input
-        class="input salt-input"
-        type="text"
-        placeholder=""
-        v-model="ciphertext"
-      />
+      <input class="input salt-input" type="text" placeholder="" v-model="ciphertext" @keyup.enter="getSeed" />
     </div>
     <div class="row">
       <button :class="['btn', btnDisabled ? 'disabled' : '']" @click="getSeed">
         {{
-          btnDisabled
-            ? 'Please enter salt value and ciphertext'
-            : 'Click To Get Your Seed'
+            btnDisabled
+              ? 'Please enter salt value and ciphertext'
+              : 'Click To Get Your Seed'
         }}
       </button>
     </div>
     <div class="row">
-      <span class="label wrap"
-        >Your Seed: <span class="tip">(Click To Copy)</span></span
-      >
+      <span class="label wrap">Your Seed: <span class="tip">(Click To Copy)</span></span>
       <div class="seed" @click="copySeed">
         {{ seed }}
+        <div class="copy-tip" v-if="showCopyTip">Copied!</div>
       </div>
     </div>
   </div>
@@ -49,10 +38,18 @@ const btnDisabled = computed(() => !salt.value || !ciphertext.value)
 function getSeed() {
   if (btnDisabled.value) return
   try {
-    let decode = atob(ciphertext.value)
     const saltBase64Part = btoa(encodeURIComponent(salt.value)).replace(/=/g, '')
-    let decodeFilter = decode.replace(saltBase64Part, '')
-    seed.value = decodeURIComponent(atob(decodeFilter))
+    const saltReverseBase64Part = btoa(encodeURIComponent(salt.value).split('').reverse().join('')).replace(/=/g, '')
+    console.log('saltBase64Part', saltBase64Part);
+    console.log('saltReverseBase64Part', saltReverseBase64Part);
+    const mix2 = atob(ciphertext.value)
+
+    const mix1Base64 = mix2.replace(saltReverseBase64Part, '')
+    const mix1 = atob(mix1Base64)
+
+    const seedBase64 = mix1.replace(saltBase64Part, '')
+
+    seed.value = decodeURIComponent(atob(seedBase64))
   } catch (err) {
     alert(
       'Encoding error, please check whether the salt value or ciphertext is correct.'
@@ -60,6 +57,7 @@ function getSeed() {
   }
 }
 
+let showCopyTip = ref(false)
 function copySeed() {
   if (!seed.value) {
     alert('Please get your seed first.')
@@ -72,7 +70,10 @@ function copySeed() {
   document.getElementById('cp_zdy_input').select()
   document.execCommand('copy')
   document.getElementById('cp_zdy_input').remove()
-  alert('Successfully Copied')
+  showCopyTip.value = true
+  setTimeout(() => {
+    showCopyTip.value = false
+  }, 2000);
 }
 </script>
 
@@ -82,33 +83,41 @@ function copySeed() {
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  min-height: 600px;
+
   .row {
     display: flex;
-    align-items: center;
+    // align-items: center;
     justify-content: space-between;
+
     @media screen and (min-width: 962px) {
       margin-bottom: 30px;
       width: 600px;
     }
+
     @media screen and (max-width: 962px) {
       margin-bottom: 20px;
       flex-direction: column;
       width: 100%;
     }
+
     .label {
+      height: 40px;
       font-size: 20px;
       display: flex;
       justify-content: flex-start;
       align-items: center;
+
       @media screen and (min-width: 962px) {
         flex: 0 0 170px;
       }
+
       @media screen and (max-width: 962px) {
         width: 100%;
         margin-bottom: 5px;
       }
     }
+
     .input {
       padding: 0 10px;
       outline: none;
@@ -116,13 +125,16 @@ function copySeed() {
       height: 40px;
       border: 1px solid #000;
       border-radius: 12px;
+
       @media screen and (min-width: 962px) {
         flex: 1;
       }
+
       @media screen and (max-width: 962px) {
         width: 100%;
       }
     }
+
     .btn {
       font-size: 20px;
       height: 40px;
@@ -131,37 +143,46 @@ function copySeed() {
       border-radius: 12px;
       outline: none;
       background-color: #000;
+
       @media screen and (min-width: 962px) {
         flex: 1;
       }
+
       @media screen and (max-width: 962px) {
         width: 100%;
       }
+
       &:active {
         background-color: #fff;
         outline: none;
         color: #000;
       }
     }
+
     .disabled {
       cursor: not-allowed;
       border-color: #000 !important;
       font-size: 16px;
+
       @media screen and (max-width: 962px) {
         font-size: 14px;
       }
+
       &:active {
         background-color: #000;
         color: #fff;
       }
     }
+
     .wrap {
       flex-wrap: wrap;
+
       .tip {
         white-space: nowrap;
         font-size: 15px;
       }
     }
+
     .seed {
       word-break: break-all;
       background-color: rgb(239, 239, 239);
@@ -170,11 +191,20 @@ function copySeed() {
       border: 1px solid #000;
       padding: 12px;
       min-height: 40px;
+      position: relative;
+
       @media screen and (min-width: 962px) {
         flex: 1;
       }
+
       @media screen and (max-width: 962px) {
         width: 100%;
+      }
+
+      .copy-tip {
+        position: absolute;
+        left: 0;
+        bottom: -30px;
       }
     }
   }
